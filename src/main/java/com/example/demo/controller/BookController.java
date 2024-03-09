65cea28cbeb21845fe73aa5929e3b3f88ed2118b
Rajasekhar M <sraja3924@gmail.com>
2024-03-09 16:25:44 +0530

    First Commit

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/getall")
+    public List<Book> getAllBooks() {
+        return bookService.findAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.findBookById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found with id " + id));
+    }
+
+    @PostMapping("/save")
+    public Book createBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
+    }
+}
+
